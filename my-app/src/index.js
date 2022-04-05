6d709f0c583569af9e5efdaf914a1e262d60a1ca
sumidaca <sumidaca@hawaii.edu>
2022-04-04 14:12:44 -1000

    started club admin home page

---
+++ my-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+class ClubAdmin extends React.Component {
+
+  render() {
+    return (
+      <h1>Home Page!</h1>
+    );
+  }
+}
+
+ReactDOM.render(<ClubAdmin />, document.getElementById('root'));
